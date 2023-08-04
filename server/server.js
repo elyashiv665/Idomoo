365d360fdb7bf3b2b94ba8dfac24ddbfd5dda147
elyashivmn0@gmail.com <elyashivmn0@gmail.com>
2023-08-04 12:57:16 +0300

    get access token

--- server/server.js
+++ server/server.js
@@ -1,15 +1,8 @@
-import mongoose from 'mongoose';
-import Video from './utils/mongo/models.js';
 import express from 'express';
+import getToken from './utils/IdomooWrapper.js'
+import dotenv from 'dotenv';
 
-// Connect to MongoDB database
-mongoose
-  .connect('mongodb://mongo/domains')
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((error) => console.error('Error connecting to MongoDB:', error));
-
-
-
+dotenv.config();
 const app = express();
 const port = 3000;
 
@@ -18,59 +11,22 @@ app.use(express.json());
 app.post('/video', async (req, res) => {
   const { color, images, email, firstName, lastName, quality, resolution } = req.body;
   try {
-    const newVideo = new Video({
-      color,
-      images,
-      email,
-      firstName,
-      lastName,
-      quality,
-      resolution,
-      status: VideoStatusEnum.SENT
-    });
-    const savedVideo = await newVideo.save();
     // TODO: add call to Idomoo 
-    res.status(201).json(savedVideo);
+    res.status(201).json({});
   } catch (err) {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
 
-app.get('/videos', async (req, res) => {
-  try {
-    const videos = await Video.find({});
-    res.json(videos);
-    // TODO: handle status
-  } catch (err) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
 
-app.get('/video/:videoId', async (req, res) => {
-  const videoId = req.params.videoId;
+app.get('/video', async (req, res) => {
   try {
-    const video = await Video.findById(videoId);
-    if (!video) {
-      return res.status(404).json({ error: 'Video not found' });
-    }
-    // TODO: handle status
-    res.json(video);
-  } catch (err) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-app.put('/video/:videoId', async (req, res) => {
-  const videoId = req.params.videoId;
-  const { color, images } = req.body;
-  try {
-    const updatedVideo = await Video.findByIdAndUpdate(videoId, { color, images }, { new: true });
-    if (!updatedVideo) {
-      return res.status(404).json({ error: 'Video not found' });
-    }
     // TODO: add call to Idomoo 
-    res.json(updatedVideo);
+    const token = await getToken();
+    console.log(token)
+    res.json({});
   } catch (err) {
+    console.error('get video failed', err);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
--- server/utils/IdomooWrapper.js
+++ server/utils/IdomooWrapper.js
@@ -1,19 +1,22 @@
-async function getToken() {
-const authHeader = `Basic ${btoa(`${process.env.ACCOUNT_ID}:${process.env.SECRET_KEY}`)}`;
+import axios from 'axios';
 
-  try {
-    const response = await fetch(process.env.IDOMOOURL, {
-      headers: {
-        Authorization: authHeader,
-      },
-    });
-    if (!response.ok) {
-      throw new Error('Failed to fetch token.');
+export default async function getToken() {
+    const authHeader = `Basic ${btoa(`${process.env.ACCOUNT_ID}:${process.env.SECRET_KEY}`)}`;
+
+    try {
+        const response = await axios.post(`${process.env.IDOMOOURL}/oauth/token`, null, {
+        headers: {
+            Authorization: authHeader
+        },
+        });
+        if (!response.statusText) {
+            throw new Error('Failed to fetch token.');
+        }
+
+        const data = response.data;
+
+        return data.access_token;
+    } catch (error) {
+        throw new Error(`Error fetching token: ${error.message}`);
     }
-
-    const data = await response.json();
-    return data.access_token; // Assuming the response has a 'token' property
-  } catch (error) {
-    throw new Error(`Error fetching token: ${error.message}`);
-  }
 }
