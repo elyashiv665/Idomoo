c0dd91f3a0dc7046cefb5b38dfd0fc1fd7b9a31d
elyashivmn0@gmail.com <elyashivmn0@gmail.com>
2023-08-04 18:43:25 +0300

    add some create video params

--- client/src/components/GenerateDialog/GenerateDialog.js
+++ client/src/components/GenerateDialog/GenerateDialog.js
@@ -1,10 +1,28 @@
 import './GenerateDialog.css';
+import MediaUploadButton from './MediaUpload/MediaUpload';
+import { useState } from 'react';
+import RGBSelect from './RGBSelect/RGBSelect'
+import TextPicker from './TextPicker/TextPicker'
 
 const GenerateDialog = () => {
+  const [selectedFiles, setSelectedFiles] = useState([]);
+  const [texts, setTexts] = useState([]);
+  const [color, setColor] = useState('#000000');
   const titleText = 'Enter the details below in order to generate your video';
+
+  const handleFilesSelect = (files) => {
+    setSelectedFiles([...selectedFiles, ...files]);
+  };
+
   return (
-      <div className='container'>
-        <div className='title'>{titleText}</div>
+      <div className='GenerateDialogContainer'>
+        <div className='GenerateDialogTitle'>{titleText}</div>
+        <div className='row'>
+          <MediaUploadButton onFilesSelect={(files) => handleFilesSelect(files)} selectedFiles={selectedFiles}/>
+          <RGBSelect color={color} setColor={(data) => setColor(data)}/>
+          <div className='colorBlock' style={{backgroundColor: color}}/>
+        </div>
+        <TextPicker texts={texts} setTexts={setTexts}/>
       </div>
     );
   };
